fa80736146b28351177cc7990d4596f3bfb62d3e
tinamanga <tinamanga@gmail.com>
2025-05-13 13:19:28 +0300

    Differentiate between variables, attributes, and properties. Use the property() function to create properties and validate input.

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,32 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Pug"):
+        self.name = name 
+        self.breed = breed  
+
+    # Name property
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # Breed property
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
